2c5bdb44e922c871c23f328a6fb478af5f8c7d53
Drake Damon <dddamon06@gmail.com>
2025-03-21 20:43:16 -0400

    set up settings for app.py

--- backend/app.py
+++ backend/app.py
@@ -4,3 +4,16 @@ from models import db, User, Routine, Exercise, RoutineExercise
 import os
 from datetime import timedelta
 from functools import wraps
+
+# creates new flask web app
+app = Flask(__name__)
+#tells to use sqlalchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout_tracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#sets up secret key for app for security
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key-for-testing')
+
+#allows other websites or apps to talk to it
+CORS(app, supports_credentials=True)
+#connects to db
+db.init_app(app)
