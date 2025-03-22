34c2d9a4711d918f03dfe94364cdbe1e4aaf4792
Drake Damon <dddamon06@gmail.com>
2025-03-21 20:12:18 -0400

    created functions for password and changing username to dictionary

--- backend/models.py
+++ backend/models.py
@@ -13,6 +13,22 @@ class User(db.Model):
   password_hash = db.Column(db.String(128), nullable=False)
 
   #Relationship routes
+  #user can have many routines and routines can look at which user it belongs
+  #lazy means dont load routines until needed
+  #cascade controls what happens to routines when changes occur
   routines = db.relationship('Routine', backref='user', lazy=True, cascade="all, delete-orphan")
 
+  #scrambles password
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+  #checks to see if password match
+  def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
   
+  #takes username and changes to key-value pairs
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'username': self.username
+    }
+
